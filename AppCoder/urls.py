a2983987971b6391361f793b2c786ac975122a6a
Santiago <santiroblesdiaz@gmail.com>
2023-12-22 00:47:48 -0300

    primer commit

---
+++ AppCoder/urls.py
@@ -0,0 +1,59 @@
+from django.urls import path
+from django.contrib.auth.views import LogoutView
+
+from AppCoder.views import (
+    autos_view,
+    autos_buscar_view,
+    autos_todos_view,
+    inicio_view,
+    profesores_view,
+    ### CRUD
+    profesores_crud_delete_view,
+    profesores_crud_read_view,
+    profesores_crud_update_view,
+    marca_view,
+    CursoCreateView,
+    CursoDetail,
+    CursoDeleteView,
+    CursoListView,
+    CursoUpdateView,
+    ### LOGIN
+    login_view,
+    registro_view,
+    ### EDITAR PERFIL
+    editar_perfil_view,
+    crear_avatar_view,
+    ### INSTRAGRAM
+    mostrar_profile_view,
+    )
+
+
+app_name = "AppCoder"
+
+urlpatterns = [
+    path("autos/", autos_view, name="autos"),
+    path("autos/todos", autos_todos_view, name="autos-todos"),
+    path("autos/buscar", autos_buscar_view, name="autos-buscar"),
+    path("comisiones/", profesores_view),
+    path("inicio/", inicio_view, name="inicio"),
+    ###### CRUD
+    path("marcas/", marca_view),
+    path("marcas-lista/", profesores_crud_read_view),
+    path("profesores-eliminar/<profesor_email>/", profesores_crud_delete_view),
+    path("profesores-editar/<profesor_email>/", profesores_crud_update_view),
+    ###### CBV
+    path("curso/list", CursoListView.as_view(), name="curso-list"),
+    path("curso/new", CursoCreateView.as_view(), name="curso-create"),
+    path("curso/<pk>", CursoDetail.as_view(), name="curso-detail"),
+    path("curso/<pk>/update", CursoUpdateView.as_view(), name="curso-update"),
+    path("curso/<pk>/delete", CursoDeleteView.as_view(), name="curso-delete"),
+    ###### LOGIN
+    path("registro", registro_view, name="registro"),
+    path("login", login_view, name="login"),
+    path("logout", LogoutView.as_view(template_name="AppCoder/logout.html"), name="logout"),
+    ###### EDITAR USUARIO
+    path("editar-perfil", editar_perfil_view, name="editar-perfil"),
+    path("crear-avatar", crear_avatar_view, name="crear-avatar"),
+    ##### INSTRAGAM
+    path("profile/<user_id>", mostrar_profile_view, name="profile"),
+]
---
+++ AppCoder/views.py
@@ -0,0 +1,338 @@
+from datetime import date
+
+from django.shortcuts import redirect, render
+
+from datetime import datetime
+from . import models
+from .models import Curso, Marca, Avatar
+
+from .forms import AutoFormulario, AutoBuscarFormulario, MarcaFormulario, UserAvatarFormulario
+
+from django.contrib.auth.decorators import login_required
+
+def inicio_view(request):
+    if request.user.is_authenticated:
+        usuario = request.user
+        avatar = Avatar.objects.filter(user=usuario).last()
+        avatar_url = avatar.imagen.url if avatar is not None else ""
+    else:
+        avatar_url = ""
+
+    return render(request, "AppCoder/inicio.html", context={"avatar_url": avatar_url})
+
+
+def autos_buscar_view(request):
+    if request.method == "GET":
+        form = AutoBuscarFormulario()
+        return render(
+            request,
+            "AppCoder/curso_formulario_busqueda.html",
+            context={"form": form}
+        )
+    else:
+        formulario = AutoBuscarFormulario(request.POST)
+        if formulario.is_valid():
+            informacion = formulario.cleaned_data
+            cursos_filtrados = []
+            for auto in Auto.objects.filter(auto=informacion["auto"]):
+                cursos_filtrados.append(auto)
+
+            contexto = {"autos": cursos_filtrados}
+            return render(request, "AppCoder/autos_list.html", contexto)
+
+
+def autos_todos_view(request):
+    todos_los_cursos = []
+    for curso in Curso.objects.all():
+        todos_los_cursos.append(curso)
+
+    contexto = {"cursos": todos_los_cursos}
+    return render(request, "AppCoder/cursos_list.html", contexto)
+
+
+def autos_view(request):
+    if request.method == "GET":
+        print("+" * 90) #  Imprimimos esto para ver por consola
+        print("+" * 90) #  Imprimimos esto para ver por consola
+        form = AutoFormulario()
+        return render(
+            request,
+            "AppCoder/auto_formulario_avanzado.html",
+            context={"form": form}
+        )
+    else:
+        formulario = AutoFormulario(request.POST)
+        if formulario.is_valid():
+            informacion = formulario.cleaned_data
+            modelo = Auto(curso=informacion["curso"], camada=informacion["camada"])
+            modelo.save()
+
+        return redirect("AppCoder:inicio")
+
+
+def profesores_view(xx):
+    nombre = "Mariano Manuel"
+    apellido = "Barracovich"
+    ahora = datetime.now()
+    diccionario = {
+        'nombre': nombre,
+        'apellido': apellido,
+        "nacionalidad": "argentino",
+        "hora": ahora,
+        "ciudades_preferidas": ["Buenos Aires", "Lima", "San Pablo", "Trieste"]
+    }  # Para enviar al contexto
+    return render(xx, "AppCoder/padre.html", diccionario)
+
+
+#### CRUD #####
+@login_required
+def marca_view(request):
+    if request.method == "GET":
+        return render(
+            request,
+            "AppCoder/profesor_formulario_avanzado.html",
+            {"form": MarcaFormulario()}
+        )
+    else:
+        formulario = MarcaFormulario(request.POST)
+        if formulario.is_valid():
+            informacion = formulario.cleaned_data
+            modelo = Marca(
+                nombre=informacion["nombre"],
+                apellido=informacion["apellido"],
+                email=informacion["email"],
+                profesion=informacion["profesion"]
+            )
+            modelo.save()
+        return render(
+            request,
+            "AppCoder/inicio.html",
+        )
+
+@login_required
+def profesores_crud_read_view(request):
+    profesores = Marca.objects.all()
+    return render(request, "AppCoder/profesores_lista.html", {"profesores": profesores})
+
+@login_required
+def profesores_crud_delete_view(request, profesor_email):
+    profesor_a_eliminar = Marca.objects.filter(email=profesor_email).first()
+    profesor_a_eliminar.delete()
+    return profesores_crud_read_view(request)
+
+@login_required
+def profesores_crud_update_view(request, profesor_email):
+    profesor = Marca.objects.filter(email=profesor_email).first()
+    if request.method == "GET":
+        formulario = MarcaFormulario(
+            initial={
+                "nombre": profesor.nombre,
+                "apellido": profesor.apellido,
+                "email": profesor.email,
+                "profesion": profesor.profesion
+            }
+        )
+        return render(request, "AppCoder/profesores_formulario_edicion.html", {"form": formulario, "profesor": profesor})
+    else:
+        formulario = MarcaFormulario(request.POST)
+        if formulario.is_valid():
+            informacion = formulario.cleaned_data
+            profesor.nombre=informacion["nombre"]
+            profesor.apellido=informacion["apellido"]
+            profesor.email=informacion["email"]
+            profesor.profesion=informacion["profesion"]
+            profesor.save()
+        return profesores_crud_read_view(request)
+
+####################  ClassBasedViews (CBV)  - Vistas basadas en Clases #########################################
+from django.views.generic import ListView, CreateView, DeleteView, UpdateView, DetailView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+
+
+class CursoListView(LoginRequiredMixin, ListView):
+    model = Curso
+    context_object_name = "cursos"
+    template_name = "AppCoder/cbv_curso_list.html"
+
+
+class CursoDetail(LoginRequiredMixin, DetailView):
+    model = Curso
+    template_name = "AppCoder/cbv_curso_detail.html"
+
+
+class CursoCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+    model = Curso
+    template_name = "AppCoder/cbv_curso_create.html"
+    success_url = reverse_lazy("AppCoder:curso-list")
+    fields = ["curso", "camada"]
+    success_message = "Curso creado con éxito!" 
+
+class CursoUpdateView(LoginRequiredMixin, UpdateView):
+    model = Curso
+    template_name = "AppCoder/cbv_curso_update.html"
+    success_url = reverse_lazy("AppCoder:curso-list")
+    fields = ["curso"]
+
+class CursoDeleteView(LoginRequiredMixin, DeleteView):
+    model = Curso
+    template_name = "AppCoder/cbv_curso_delete.html"
+    success_url = reverse_lazy("AppCoder:curso-list")
+
+
+
+#################### CLASE 23:  Login / Logout #########################################
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate
+
+def login_view(request):
+
+    if request.user.is_authenticated:
+        return render(
+            request,
+            "AppCoder/inicio.html",
+            {"mensaje": f"Ya estás autenticado: {request.user.username}"}
+        )
+
+    if request.method == "GET":
+        return render(
+            request,
+            "AppCoder/login.html",
+            {"form": AuthenticationForm()}
+        )
+    else:
+        formulario = AuthenticationForm(request, data=request.POST)
+
+        if formulario.is_valid():
+            informacion = formulario.cleaned_data
+            usuario = informacion["username"]
+            password = informacion["password"]
+
+            modelo = authenticate(username=usuario, password=password)
+            login(request, modelo)
+
+            return render(
+                request,
+                "AppCoder/inicio.html",
+                {"mensaje": f"Bienvenido {modelo.username}"}
+            )
+        else:
+            return render(
+                request,
+                "AppCoder/login.html",
+                {"form": formulario}
+            )
+
+
+
+def logout_view(request):
+    pass
+
+
+from .forms import UserCreationFormulario, UserEditionFormulario
+from django.contrib.auth.views import PasswordChangeView
+
+
+def registro_view(request):
+
+    if request.method == "GET":
+        return render(
+            request,
+            "AppCoder/registro.html",
+            {"form": UserCreationFormulario()}
+        )
+    else:
+        formulario = UserCreationFormulario(request.POST)
+        if formulario.is_valid():
+            informacion = formulario.cleaned_data
+            usuario = informacion["username"]
+            formulario.save()
+
+            return render(
+                request,
+                "AppCoder/inicio.html",
+                {"mensaje": f"Usuario creado: {usuario}"}
+            )
+        else:
+            return render(
+                request,
+                "AppCoder/registro.html",
+                {"form": formulario}
+            )
+
+#################### CLASE 24:  Editar perfil #########################################
+
+@login_required
+def editar_perfil_view(request):
+
+    usuario = request.user
+    avatar = Avatar.objects.filter(user=usuario).last()
+    avatar_url = avatar.imagen.url if avatar is not None else ""
+
+
+    if request.method == "GET":
+
+
+        valores_iniciales = {
+            "email": usuario.email,
+            "first_name": usuario.first_name,
+            "last_name": usuario.last_name
+        }
+
+
+        formulario = UserEditionFormulario(initial=valores_iniciales)
+        return render(
+            request,
+            "AppCoder/editar_perfil.html",
+            context={"form": formulario, "usuario": usuario, "avatar_url": avatar_url}
+            )
+    else:
+        formulario = UserEditionFormulario(request.POST)
+        if formulario.is_valid():
+            informacion = formulario.cleaned_data
+
+            usuario.email = informacion["email"]
+
+            usuario.set_password(informacion["password1"])
+
+            usuario.first_name = informacion["first_name"]
+            usuario.last_name = informacion["last_name"]
+            usuario.save()
+        return redirect("AppCoder:inicio")
+
+
+@login_required
+def crear_avatar_view(request):
+
+    usuario = request.user
+
+    if request.method == "GET":
+        formulario = UserAvatarFormulario()
+        return render(
+            request,
+            "AppCoder/crear_avatar.html",
+            context={"form": formulario, "usuario": usuario}
+        )
+    else:
+        formulario = UserAvatarFormulario(request.POST, request.FILES)
+        if formulario.is_valid():
+            informacion = formulario.cleaned_data
+            modelo = Avatar(user=usuario, imagen=informacion["imagen"])
+            modelo.save()
+            return redirect("AppCoder:inicio")
+
+#################### CLASE 25:  INSTRAGRAM #########################################
+
+from django.contrib.auth.models import User
+from .models import Post, Perfil
+
+def mostrar_profile_view(request, user_id):
+    user = User.objects.get(id=user_id)
+    todos_los_posts = Post.objects.filter(autor=user).all()
+    perfil = Perfil.objects.filter(usuario=user).first()
+    avatar = Avatar.objects.filter(user=user).first()
+    avatar_url = avatar.imagen.url if avatar is not None else ""
+    contexto = {"posts": todos_los_posts, "perfil": perfil, "avatar_url": avatar_url}
+
+    return render(request, "AppCoder/instagram_profile.html", context=contexto)
